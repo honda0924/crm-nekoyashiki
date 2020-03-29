31d6b1a592ea75262d30a69df53dae5b6be44997
honda0924 <honda0924@hotmail.co.jp>
2020-03-29 15:48:33 +0900

    migration_and_write_readme

--- README.md
+++ README.md
@@ -1,3 +1,52 @@
+# CRM-Nekoyashiki DB設計
+
+## usersテーブル
+|Column|Type|Options|
+|------|----|-------|
+|name|string|null: false|
+|email|string|null: false|
+|password|string|null: false|
+### Association
+- has_many :clients
+
+## clientsテーブル
+|Column|Type|Options|
+|------|----|-------|
+|client_name1|string|null: false,index: true|
+|client_name2|string||
+|postcode|string||
+|add1|string||
+|add2|string||
+|add3|string||
+|tel|string||
+|fax|string||
+|client_person_in_charge|string||
+|mobile|string||
+|url|string||
+|email|string||
+|client_rank|integer|active_hash|
+|client_cat|integer|active_hash|
+|closing_day|integer|active_hash|
+|payment_day|integer|active_hash|
+|payment_method|integer|active_hash|
+|user|references|foreign_key: true|
+### Association
+- has_many :contacts
+- belongs_to :user
+
+## contactsテーブル
+|Column|Type|Options|
+|------|----|-------|
+|act_cat|integer|active_hash|
+|detail|text|null: false|
+|start_date|datetime||
+|end_date|datetime||
+|in_progress|boolean||
+|client|references|foreign_key: true|
+### Association
+- belongs_to :client
+
+
 # README
 
 This README would normally document whatever steps are necessary to get the
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,43 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_29_060419) do
+ActiveRecord::Schema.define(version: 2020_03_29_063132) do
+
+  create_table "clients", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "client_name1"
+    t.string "client_name2"
+    t.string "postcode"
+    t.string "add1"
+    t.string "add2"
+    t.string "add3"
+    t.string "tel"
+    t.string "fax"
+    t.string "client_person_in_charge"
+    t.string "mobile"
+    t.string "url"
+    t.string "email"
+    t.integer "client_rank"
+    t.integer "client_cat"
+    t.integer "closing_day"
+    t.integer "payment_day"
+    t.integer "payment_method"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_clients_on_user_id"
+  end
+
+  create_table "contacts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "act_cat"
+    t.text "detail"
+    t.datetime "start_date"
+    t.datetime "end_date"
+    t.boolean "in_progress"
+    t.bigint "client_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["client_id"], name: "index_contacts_on_client_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -20,8 +56,11 @@ ActiveRecord::Schema.define(version: 2020_03_29_060419) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "clients", "users"
+  add_foreign_key "contacts", "clients"
 end
