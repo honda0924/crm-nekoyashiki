75a1fd0e2b8b4fa675a7f5d226a780e87470a83d
honda0924 <honda0924@hotmail.co.jp>
2020-05-03 15:07:54 +0900

    client_info

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -7,6 +7,9 @@ class ClientsController < ApplicationController
     @client =Client.create(client_params)
     redirect_to root_path
   end
+  def show
+    @client =Client.find(params[:id])
+  end
   private
   def client_params
     params.require(:client).permit(:client_name1,:client_name2,:postcode,:addr1,:addr2,:addr3,:tel,:fax,:client_person_in_charge,:mobile,:url,:email,:client_rank_id,:client_cat_id,:closing_day_id,:payment_day_id,:payment_method_id,:user_id)
